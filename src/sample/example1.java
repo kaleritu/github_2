c9d53aeb8147faf3cfdc2b9a784d27503ad2068c
DURGA <DURGA@Akash>
2021-08-01 15:24:26 +0530

    [ritu]:project created

---
+++ src/sample/example1.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class example1 {
+public static void main(String[] args) {
+	System.out.println("hii");
+	System.out.println("good morning");
+}
+}
---
+++ src/sample/example2.java
@@ -0,0 +1,8 @@
+package sample;
+
+public class example2 {
+public static void main(String[] args) {
+	System.out.println("google");
+	System.out.println("doodle");
+}
+}
